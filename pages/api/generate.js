f051ff3b5e17a6611a5199866509a33d7f8631e6
hogyzen12 <billypapas12@gmail.com>
2022-12-10 16:14:34 +0100

    updating prompt to handle multi entry

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -7,9 +7,9 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 const basePromptPrefix =
 `
-The user will provide you with a book they loved, in the form of the User Input. Reccomend to the user 3 books they should read based on their inputted book. For each book reccomendation made to the user make sure to summarise the reccomendation with 4 sentences. Add an extra sentence that mentions some of the negatives of the book. Give each reccomendation a rating out of 5. Append how many pages the book is.
+The user will provide you with one or more anime series they loved, in the form of the User Input. Reccomend to the user 3 anime they should watch based on their inputted anime. For each anime reccomendation made to the user make sure to summarise the reccomendation with 4 sentences. Add an extra sentence that mentions some of the negatives of the anime. Give each reccomendation a rating out of 5. Append if the anime is dubbed or not, and the number of seasons and episodes.
 
-User Input: 
+User Input:
 `
 const generateAction = async (req, res) => {
   // Run first prompt
@@ -18,7 +18,7 @@ const generateAction = async (req, res) => {
   const baseCompletion = await openai.createCompletion({
     model: 'text-davinci-003',
     prompt: `${basePromptPrefix}${req.body.userInput}\n`,
-    temperature: 0.85,
+    temperature: 0.9,
     max_tokens: 690,
   });
   
@@ -32,3 +32,7 @@ export default generateAction;
 //The user will provide you with an anime they loved, in the form of the User Input. Reccomend to the user 3 anime they should watch based on their inputted anime. For each anime reccomendation made to the user make sure to summarise the reccomendation with 4 sentences. Add an extra sentence that mentions some of the negatives of the anime. Give each reccomendation a rating out of 5. Append if the anime is dubbed or not, and the number of seasons and episodes.
 
 //User Input:
+
+//The user will provide you with a book they loved, in the form of the User Input. Reccomend to the user 3 books they should read based on their inputted book. For each book reccomendation made to the user make sure to summarise the reccomendation with 4 sentences. Add an extra sentence that mentions some of the negatives of the book. Give each reccomendation a rating out of 5. Append how many pages the book is.
+
+//User Input: 
